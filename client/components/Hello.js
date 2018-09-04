30190dea6f695a1ff2bb93e137af84f4470ba823
Radoslaw Weidemann <walecznyopos@gmail.com>
2018-09-04 12:17:56 +0200

    mern

---
+++ client/components/Hello.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Hello extends Component {
+  render() {
+    return (
+    );
+  }
+}
+
+
+
+export default Hello;
---
+++ components/Hello/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const  Hello = (props, context)  => {
+  return (
+  );
+}
+
+Hello.propTypes = {
+};
+
+export default Hello;
