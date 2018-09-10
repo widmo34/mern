d53baaf9d9ca5b81e32b5f5357c90ad5f45f5e0e
Radoslaw Weidemann <walecznyopos@gmail.com>
2018-09-11 00:02:51 +0200

    example c almost finish

--- client/routes.js
+++ client/routes.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './modules/App/App';
 
+
 // require.ensure polyfill for node
 if (typeof require.ensure !== 'function') {
   require.ensure = function requireModule(deps, callback) {
@@ -18,6 +19,7 @@ if (process.env.NODE_ENV !== 'production') {
   // Require async routes only in development for react-hot-reloader to work.
   require('./modules/Post/pages/PostListPage/PostListPage');
   require('./modules/Post/pages/PostDetailPage/PostDetailPage');
+  require('./modules/Home/Home')
 }
 
 // react-router setup with code-splitting
@@ -39,5 +41,21 @@ export default (
         });
       }}
     />
+
+ <Route
+  path="/home"
+  getComponent={(nextState, cb) => {
+    require.ensure([], require => {
+      cb(null, require('./modules/Home/Home').default);
+    });
+  }}
+/>
+
+  
+
   </Route>
+
+  
+
+  
 );
